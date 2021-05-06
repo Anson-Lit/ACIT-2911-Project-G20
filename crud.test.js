e69a6e05ade13e3e9e505c0bfc502fe143d7c272
Aaron <aaronlhma@gmail.com>
2021-05-06 09:54:01 -0700

    started on testing create route

--- crud.test.js
+++ crud.test.js
@@ -1,11 +1,43 @@
 //unit tests for CRUD functionality
 
-// testing to see if jest is working properly
+// requiring the app
+const app = require('./index');
+const request = require('supertest');
 
-it('Testing jest, test pass', () => {
+beforeAll((done) => {
+    done();
+});
+
+afterAll( async (done) => {
+    app.close();
+    done();
+});
+
+
+const test1 = {
+    name: "test",
+    email: "test@email.com",
+    transaction: "test transaction",
+    price: 40
+}
+
+
+
+it('Testing jest', () => {
     expect(1).toBe(1)
 })
 
-it('Testing jest, test fail', () => {
-    expect(1).toBe(2)
-})
+
+// successfully create a new user that doesnt already exist
+it('Testing to create a new nonexistent user', async () => {
+    const res = await request(app)
+        .post('/expense')
+        .send(test1)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+        //the database should give the user an id and date automatically
+        expect(res.body.id).toBeDefined()
+        expect(res.body.date).toBeDefined()
+});
