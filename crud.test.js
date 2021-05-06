c2c4cfea2ea3a4a47af5d479cc381c774953d7b6
Aaron <aaronlhma@gmail.com>
2021-05-05 17:26:33 -0700

    testing jest functionality

--- crud.test.js
+++ crud.test.js
@@ -1,1 +1,11 @@
 //unit tests for CRUD functionality
+
+// testing to see if jest is working properly
+
+it('Testing jest, test pass', () => {
+    expect(1).toBe(1)
+})
+
+it('Testing jest, test fail', () => {
+    expect(1).toBe(2)
+})
