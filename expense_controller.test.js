a7611aad551ee4d210a91b627614588521fc0578
Anson <ansonlit@live.ca>
2021-05-20 18:51:37 -0700

    Reverted test back to normal after demoing for circleCI

--- expense_controller.test.js
+++ expense_controller.test.js
@@ -52,7 +52,7 @@ const returnVal = {
         date: '1',
         transaction: 'apple',
         cost: "40",
-        tags: "Foods",
+        tags: "Food",
         userId: "082cbf6d-b6cd-40dd-bade-891d89ce8611"
     }
 }
